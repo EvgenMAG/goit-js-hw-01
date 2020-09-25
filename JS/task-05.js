08b7c84896b9c6a5e3da05d258c0a18dea70786b
Evgeniy Chernyshenko <magdegaben@gmail.com>
2020-09-25 13:49:08 +0300

    adding task 5

--- JS/task-05.js
+++ JS/task-05.js
@@ -11,3 +11,44 @@
 // Австралия - 170 кредитов
 // Индия - 80 кредитов
 // Ямайка - 120 кредитов
+
+const China = "Китай";
+const Chili = "Чили";
+const Australia = "Австралия";
+const India = "Индия";
+const Jamayka = "Ямайка";
+const CANCELED_BY_USER = "Отменено пользователем!";
+const NO_DELIVERY = "В выбранную страну доставка недоступна.";
+let message = "";
+let country = "";
+let price;
+
+let countryName = prompt("Укажите страну доставки", "");
+if (countryName === null) {
+  message = CANCELED_BY_USER;
+} else if (countryName == "") {
+  message = "You forgot to enter the country ";
+} else {
+  country = countryName[0].toUpperCase() + countryName.slice(1).toLowerCase();
+  switch (country) {
+    case China:
+      price = 100;
+      break;
+
+    case Chili:
+      price = 250;
+      break;
+
+    case Australia:
+      price = 170;
+      break;
+    default:
+      console.log(NO_DELIVERY);
+  }
+}
+
+if (price > 0) {
+  message = `Доставка в ${country} будет стоить ${price} кредитов`;
+}
+
+console.log(message);
